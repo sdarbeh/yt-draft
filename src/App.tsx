25090bdc8cd0d8d1bedf9ea70038262ce7509e07
Sam Darbeh <56693052+sdarbeh@users.noreply.github.com>
2021-03-01 18:57:09 -0600

    add hash routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { client } from './utils/routes/client';
 // routes
 import Client from './app/client/Route'
